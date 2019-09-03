c67c392e47ac768fc4e1374305933a3c81a940c0
qxinzheng <312782144.qq.com>
2019-09-03 17:58:10 +0800

    初建项目

---
+++ app/src/main/java/com/example/toolbar/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private SimpleToolbar mSimpleToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSimpleToolbar = findViewById(R.id.simple_toolbar);
+
+        mSimpleToolbar.setLeftTitleClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "点击了左侧Title", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mSimpleToolbar.setRightTitleClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "点击了右侧Title", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
